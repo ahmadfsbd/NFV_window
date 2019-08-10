2e3531cb89f1f8c07e1bc2a00d73533149b16b8c
ahmedbutt7121991 <asifengin33r@gmail.com>
2019-08-10 18:47:32 -0500

    added code for stack clean

--- nfv-auto/stack_creation_failed_clean.sh
+++ nfv-auto/stack_creation_failed_clean.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 counter=0
 line=''
+check_power_status()
+{   echo "================ Checking Power status of nOdes ========================"
+    echo "================ Checking Power status of nOdes ========================">> node_clean.log
+  output=$(ironic node-show "($1)")
+  echo "$output"
+  power_status=$(echo "$output" | awk -F "|" '/power status/ {print $1}')
+  echo "$power_statust"
+  echo "$power_status" >> node_clean.log
+  counter=$(($counter+1))
+  if [ $1='power on' ]
+  then
+    echo "Node $counter is ON"
+  else
+    echo "Node $counter is OFF"
+  fi
+  echo "============================================================================================================="
+  echo "=============================================================================================================" >> node_clean.log
+}
+
 setting_Maintenance()
 {   echo "================ Setting Node Maintenanace MOde to True ========================"
     echo "================ Setting Node Maintenanace MOde to True ========================">> node_clean.log
@@ -24,13 +43,21 @@ deleting_node()
 echo "================================ Node Clean Log File =====================" > node_clean.log
 output=$(ironic node-list)
 node_uuids=$(echo "$output" | awk -F "|" '/-/ { print $1}')
+power_status=$(echo "$output" | awk -F "|" '/-/ { print $1}')
 echo "$node_uuids" > node_uuid_list.txt
-
+echo "$power_status" > node_power_status.txt
+#=============maintenanace and deletion==========
 filename="node_uuid_list.txt"
 while read -r line; do
-  setting_Maintenance $line
-  deleting_node $line
+#  setting_Maintenance $line
+#  deleting_node $line
+   echo "Node uuid is $line"
 done < "$filename"
 
-cd
-rm -rf instackenv.json
+#=============== power check ========================
+filename="node_power_status.txt"
+while read -r line; do
+    check_power_status $line
+done < "$filename"
+#cd
+#rm -rf instackenv.json
